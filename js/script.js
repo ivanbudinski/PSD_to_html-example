e66df1e819768a77587195671660df7f3effdb96
ivan budinski <budinskiivan@gmail.com>
2018-01-20 09:59:16 +0100

    PSD to html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    document.getElementById("myDropdown").classList.remove("show");
+  }
+}
